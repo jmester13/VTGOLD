be8eba6da437d70c09661ad8f67de80959ddbf16
Joseph Mester <joe@joemester.com>
2021-02-22 20:56:58 -0500

    accordiona and zrow work

--- web/themes/custom/vermontgold/js/script.js
+++ web/themes/custom/vermontgold/js/script.js
@@ -8,5 +8,33 @@
 (function ($, Drupal) {
   'use strict';
 
+   // Accordion
+   $(".paragraph.paragraph--type--accordion .field.field--name-field-accordion-item.field__items .field__item:first-child .paragraph.paragraph--type--accordion-item").addClass("active");
+   $(".paragraph--type--accordion-item").on("click", function() {
+     if ($(this).hasClass("active")) {
+       $(this).removeClass("active");
+       $(this)
+         .siblings(".paragraph.paragraph--type--accordion.field.field--name-field-paragraphs")
+         .slideUp(200);
+       $(".paragraph--type--accordion-item > .field__item i")
+         .removeClass("fa-minus")
+         .addClass("fa-plus");
+     } else {
+       $(".paragraph--type--accordion-item > .field__item i")
+         .removeClass("fa-minus")
+         .addClass("fa-plus");
+       $(this)
+         .find("i")
+         .removeClass("fa-plus")
+         .addClass("fa-minus");
+       $(".paragraph--type--accordion-item").removeClass("active");
+       $(this).addClass("active");
+       $(".paragraph.paragraph--type--accordion.field.field--name-field-paragraphs").slideUp(200);
+       $(this)
+         .siblings(".paragraph.paragraph--type--accordion.field.field--name-field-paragraphs")
+         .slideDown(200);
+     }
+   });
+
 
 })(jQuery, Drupal);

0f850fef77e0a6180292514b89151728a7e5b16b
Joseph Mester <joe@joemester.com>
2021-04-01 19:25:45 -0400

    testimonial bg color transparency

--- web/themes/custom/vermontgold/js/script.js
+++ web/themes/custom/vermontgold/js/script.js
@@ -37,13 +37,27 @@
    });
 
    // Same Height
-   var maxHeight = 0;
-
-    $(".views-col").each(function(){
-      if ($(this).height() > maxHeight) { maxHeight = $(this).height(); }
-    });
-
-    $(".views-col").height(maxHeight);
+   (function($) {
+   // Select and loop the container element of the elements you want to equalise
+    $('.views-row').each(function(){  
       
+      // Cache the highest
+      var highestBox = 0;
+      
+      // Select and loop the elements you want to equalise
+      $('.views-col', this).each(function(){
+        
+        // If this box is higher than the cached highest then store it
+        if($(this).height() > highestBox) {
+          highestBox = $(this).height(); 
+        }
+      
+      });  
+            
+      // Set the height of all those children to whichever was highest 
+      $('.views-col',this).height(highestBox);
+                    
+    }); 
+  })(jQuery);
 
 })(jQuery, Drupal);

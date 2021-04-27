50b3e6e9bcf9f8721dac73678a50e2e90bfde354
Joseph Mester <joe@joemester.com>
2021-04-27 16:37:41 -0400

    prod coa colorbox img size

--- web/themes/custom/vermontgold/js/script.js
+++ web/themes/custom/vermontgold/js/script.js
@@ -46,6 +46,22 @@
   //   });
   // });
 
+  var cboxOptions = {
+    width: '95%',
+    height: '95%',
+    maxWidth: '960px',
+    maxHeight: '960px',
+  }
+  
+  $('.cbox-link').colorbox(cboxOptions);
+  
+  $(window).resize(function(){
+      $.colorbox.resize({
+        width: window.innerWidth > parseInt(cboxOptions.maxWidth) ? cboxOptions.maxWidth : cboxOptions.width,
+        height: window.innerHeight > parseInt(cboxOptions.maxHeight) ? cboxOptions.maxHeight : cboxOptions.height
+      });
+  });
+
    // Same Height
    (function($) {
     setTimeout(function() { 

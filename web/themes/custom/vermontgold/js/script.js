f4d09b5667aa9df224305de423b3d9d4d579a07f
Joseph Mester <joe@joemester.com>
2021-05-04 08:18:46 -0400

    header work

--- web/themes/custom/vermontgold/js/script.js
+++ web/themes/custom/vermontgold/js/script.js
@@ -36,6 +36,34 @@
      }
    });
 
+   $('.header .main-menu .region-navigation .menu--main div.view-mode-mobile-only').closest('li.menu-item').addClass('view-mode-mobile-only');
+
+   // Third level drop down
+       $(".menu-dropdown-1 .menu.menu-level-2 .menu-item.menu-item--expanded").click(function(){
+           $(this).toggleClass("clicked");
+       });
+   
+       $(".menu-dropdown-1 .menu.menu-level-2 .menu-item.menu-item--expanded.clicked").click(function(){
+           $(this).removeClass("clicked");
+       });
+   
+       // Mobile
+       // Hide Menu
+       $('.header .main-menu .region-navigation .menu--main ul.menu').addClass('hide');
+       
+       // Show search on click
+       $('.header .main-menu .mobile .hamburger-menu .bar').on('click', function() {
+           $('form#search-block-form').toggleClass('visible');
+       })
+       
+       // Menu Show
+       $('.hamburger-menu').on('click', function() {
+       $('.bar').toggleClass('animate');
+           var mobileNav = $('.header .main-menu .region-navigation .menu--main ul.menu');
+           mobileNav.toggleClass('hide show');
+       }) 
+     
+
 
    // Same Height
    (function($) {
@@ -62,36 +90,5 @@
     }); 
   }, 400);
   })(jQuery);
-
-  (function($) {
-    setTimeout(function() { 
-    $('.header .main-menu .region-navigation .menu--main div.view-mode-mobile-only').closest('li.menu-item').addClass('view-mode-mobile-only');
-
-// Third level drop down
-    $(".menu-dropdown-1 .menu.menu-level-2 .menu-item.menu-item--expanded").click(function(){
-        $(this).toggleClass("clicked");
-    });
-
-    $(".menu-dropdown-1 .menu.menu-level-2 .menu-item.menu-item--expanded.clicked").click(function(){
-        $(this).removeClass("clicked");
-    });
-
-    // Mobile
-    // Hide Menu
-    $('.header .main-menu .region-navigation .menu--main ul.menu').addClass('hide');
-    
-    // Show search on click
-    $('.header .main-menu .mobile .hamburger-menu .bar').on('click', function() {
-        $('form#search-block-form').toggleClass('visible');
-    })
-    
-    // Menu Show
-    $('.hamburger-menu').on('click', function() {
-		$('.bar').toggleClass('animate');
-        var mobileNav = $('.header .main-menu .region-navigation .menu--main ul.menu');
-        mobileNav.toggleClass('hide show');
-    }) 
-  }, 600);
-})(jQuery);
 
 })(jQuery, Drupal);

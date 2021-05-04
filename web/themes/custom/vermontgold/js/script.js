e73ed9f1993a0dc04783b78cec1e8ee3918b0fe6
Joseph Mester <joe@joemester.com>
2021-05-04 13:30:44 -0400

    coa lightbox work

--- web/themes/custom/vermontgold/js/script.js
+++ web/themes/custom/vermontgold/js/script.js
@@ -35,6 +35,35 @@
          .slideDown(200);
      }
    });
+   
+  //  // COA popup
+  // $('.coas .field__item').each(function(){
+  //   $(this).find('field__item').on('click',function(){
+  //     $(this).parents('.field__item').addClass('show');
+  //   });
+  //   $(this).find('.close').on('click',function(){
+  //     $(this).parents('.field__item').removeClass('show');
+  //   });
+  // });
+
+   var cboxOptions = {
+    width: '95%',
+    height: '95%',
+    maxWidth: '960px',
+    maxHeight: '960px',
+  };
+  
+  $('.cbox-link').colorbox(cboxOptions);
+  
+  $(window).resize(function(){
+      $.colorbox.resize({
+        width: window.innerWidth > parseInt(cboxOptions.maxWidth) ? cboxOptions.maxWidth : cboxOptions.width,
+        height: window.innerHeight > parseInt(cboxOptions.maxHeight) ? cboxOptions.maxHeight : cboxOptions.height
+      });
+  });
+
+
+   // Mobile Menu
 
    $('.header .main-menu .region-navigation .menu--main div.view-mode-mobile-only').closest('li.menu-item').addClass('view-mode-mobile-only');
 

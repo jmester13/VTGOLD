e1e4008d9b617cf4163832149e50dfcecbac3dd5
Joe Mester <joe@joemester.com>
2021-05-25 10:35:55 -0400

    IG update with test feed, update modules via composer

--- web/themes/custom/vermontgold/js/instagram.js
+++ web/themes/custom/vermontgold/js/instagram.js
@@ -2,21 +2,35 @@
     'use strict';
     jQuery(function($) {
         $(document).ready(function () {
+
+            var feed = new Instafeed({
+                accessToken: 'IGQVJXbmdndVU0N1Q4QUNpSkJRdW5fNU9vdWlqMFkwSXNnN2ppYU5GY0huOHlmQnhPZAWF1RmN4QjRFSUlCbnU5Tm9VX1VDSXFLQThhUy1pUHlycWJkc1YzUVdqMENqLTZADd0RraFNCNkJ3RWVwVXVBaAZDZD',
+                limit: 7,
+                template: '<li><a href="{{link}}"><img title="{{caption}}" src="{{image}}" /></a></li>',
+              });
+              feed.run();
+
+            var feed = new Instafeed({
+                accessToken: 'your-token'
+              });
+              feed.run();
+
+
         
-        $.instagramFeed({
-            'host':'https://images' + ~~(Math.random() * 3333) + '-focus-opensocial.googleusercontent.com/gadgets/proxy?container=none&url=https://www.instagram.com/',
-            'username': 'thevermontgoldco',
-            'container': "#instagram-data",
-            'display_profile': false,
-            'display_biography': false,
-            'display_gallery': true,
-            'callback': null,
-            'styling': true,
-            'image_size': 820,
-            'items': 10,
-            'items_per_row': 10,
-            'cache_time':120
-        });
+        // $.instagramFeed({
+        //     'host':'https://images' + ~~(Math.random() * 3333) + '-focus-opensocial.googleusercontent.com/gadgets/proxy?container=none&url=https://www.instagram.com/',
+        //     'username': 'thevermontgoldco',
+        //     'container': "#instagram-data",
+        //     'display_profile': false,
+        //     'display_biography': false,
+        //     'display_gallery': true,
+        //     'callback': null,
+        //     'styling': true,
+        //     'image_size': 820,
+        //     'items': 10,
+        //     'items_per_row': 10,
+        //     'cache_time':120
+        // });
         // Custom Block Slider
         $('.instagram_gallery').slick({
             infinite: true,
